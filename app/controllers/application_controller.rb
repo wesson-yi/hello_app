809bd24fb92e4a019f6aedfbde30f27dffce8efb
wesson.yi <wesson.yi@wq-wesson-yi.local>
2016-09-30 09:41:35 +0800

    Hello,mundo!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "hello,world!"
+    render text: "hello,mundo!"
   end
 end
+
