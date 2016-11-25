ff5ba20105b52b774ab781d23e37708277be7ead
wesson.yi <wesson.yi@wq-wesson-yi.local>
2016-11-25 16:01:51 +0800

    复习 hello_app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  get 'hello',    to: 'application#hello'
+  get 'goodbye',  to: 'application#goodbye'
 
   get '/home',    to: 'static_pages#home'
   get '/about',   to: 'static_pages#about'
