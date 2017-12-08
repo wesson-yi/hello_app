064ecd0e8c4bb4f8af89d213acf6193f49575136
wesson-yi <wesson_yi@163.com>
2017-12-08 23:20:33 +0800

    分离查询对象 Query Objects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,14 @@ class ApplicationController < ActionController::Base
   def goodbye
     render html: "good bye!"
   end
+
+  def send_offer
+    AbandonedTrialQuery.new.find_each do |account|
+      account.send_off_for_support
+    end
+
+    account = Account.where("created_at < ?", 1.month.ago)
+    abandoned_trials = AbandonedTrialQuery.new(account)
+  end
 end
 
